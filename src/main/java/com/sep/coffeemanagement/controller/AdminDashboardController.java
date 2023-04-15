cabf82da88feeb0c7f9d2125635258872f5ac7e2
phamducanhh <ntduong2792001@gmail.com>
2023-04-15 04:46:07 -0700

    anhpd fix api availabe for branch manager

--- src/main/java/com/sep/coffeemanagement/controller/AdminDashboardController.java
+++ src/main/java/com/sep/coffeemanagement/controller/AdminDashboardController.java
@@ -6,9 +6,11 @@ import com.sep.coffeemanagement.dto.branch.BranchRes;
 import com.sep.coffeemanagement.dto.common.CommonResponse;
 import com.sep.coffeemanagement.dto.common.ListWrapperResponse;
 import com.sep.coffeemanagement.service.admin_dashboard.AdminDashboardService;
+import com.sep.coffeemanagement.service.branch.BranchService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "admin-dashboard")
 public class AdminDashboardController extends AbstractController<AdminDashboardService> {
+  @Autowired
+  private BranchService branchService;
 
   @SecurityRequirement(name = "Bearer Authentication")
   @GetMapping(value = "get-list-branch-revenue")
@@ -29,7 +33,18 @@ public class AdminDashboardController extends AbstractController<AdminDashboardS
     @RequestParam(required = false, defaultValue = "") String sortField,
     HttpServletRequest request
   ) {
-    validateAuthorize(request, new String[] { Constant.ADMIN_ROLE });
+    validateAuthorize(
+      request,
+      new String[] { Constant.ADMIN_ROLE, Constant.BRANCH_ROLE }
+    );
+    String userId = checkAuthentication(request);
+    String role = getUserRoleByRequest(request);
+    if (Constant.BRANCH_ROLE.equals(role)) {
+      allParams.put(
+        "branchId",
+        branchService.getBranchByManagerId(userId).get().getBranchId()
+      );
+    }
     allParams.put("status", Constant.ORDER_STATUS_SUMMARY_REVENUE);
     return response(
       service.getListBranchRevenue(allParams, keySort, page, pageSize, sortField),
