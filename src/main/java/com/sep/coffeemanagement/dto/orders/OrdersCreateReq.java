546bcc4052e14ad8b88a2e9c814eaff2569c2d13
phamducanhh <ntduong2792001@gmail.com>
2023-04-18 02:10:01 -0700

    anhpd add not blank not empty constrain to ordersCreateReq

--- src/main/java/com/sep/coffeemanagement/dto/orders/OrdersCreateReq.java
+++ src/main/java/com/sep/coffeemanagement/dto/orders/OrdersCreateReq.java
@@ -2,6 +2,8 @@ package com.sep.coffeemanagement.dto.orders;
 
 import com.sep.coffeemanagement.dto.order_detail.OrderDetailReq;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,16 +20,24 @@ public class OrdersCreateReq {
   private String branchId;
 
   @NotNull(message = "address is null")
+  @NotBlank(message = "address is null")
+  @NotEmpty(message = "address is null")
   @Length(max = 2000, message = "address over length(2000)")
   private String address;
 
   @NotNull(message = "province is null")
+  @NotBlank(message = "province is null")
+  @NotEmpty(message = "province is null")
   private String province;
 
   @NotNull(message = "ward is null")
+  @NotBlank(message = "ward is null")
+  @NotEmpty(message = "ward is null")
   private String ward;
 
   @NotNull(message = "district is null")
+  @NotBlank(message = "district is null")
+  @NotEmpty(message = "district is null")
   private String district;
 
   private String couponId;
